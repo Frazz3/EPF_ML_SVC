195c438819328258292b8eb764ba241a7ac63741
Sebastian Franzén <sebastian@n148-p82.eduroam.kth.se>
2019-04-26 11:56:14 +0200

    added 10 fold cross validation and calculated the mean value of accuracy.

--- svc_elec.py
+++ svc_elec.py
@@ -1,8 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import svm, preprocessing
+from sklearn.model_selection import KFold, cross_val_score
 import pandas as pd
 from matplotlib import style
+from math import floor
+import statistics
 style.use("ggplot")
 
 FEATURES = ["date","period","nswprice","nswdemand"]
@@ -44,9 +47,11 @@ FEATURES = ["date","period","nswprice","nswdemand"]
 
 def Build_Data_Set():
     data_df = pd.read_csv("electricity-normalized.csv")
+    data_df = classify(data_df)
     #data_df = pd.read_csv("key_stats.csv")
 
     X = np.array(data_df[FEATURES].values)#.tolist())
+    #k_fold = KFold(n_splits=10)
 
     #y = (data_df["Status"].replace("outperform", 1).replace("underperform", 0).values.tolist())
     y = (data_df["class"].replace("UP", 1).replace("DOWN", 0).values.tolist())
@@ -55,25 +60,52 @@ def Build_Data_Set():
 
     return X,y
 
+def classify(df):
+    prev_price = 0
+    for i,r in df.iterrows():
+        try:
+            if r['nswprice'] <= df.at[i+1,'nswprice']:
+                df.at[i,'class'] = 'UP'
+            else:
+                df.at[i,'class'] = 'DOWN'
+        except:
+            df.at[i,'class'] = 'DOWN'
+
+
+    #print(df.head())
+    return df
+
 def Analysis():
 
-    test_size = 500
+
     X, y = Build_Data_Set()
-    print(len(X))
+    svc = svm.SVC(C=1, kernel='linear')
+    k_fold = KFold(n_splits=10)
+
+    # for train_indices, test_indices in k_fold.split(X):
+    #     print ('Train: %s | test: %s' %(train_indices, test_indices))
+
+    crossVal = cross_val_score(svc, X, y, cv=k_fold, n_jobs=-1)
+    print(crossVal)
+    print('Mean value: %s' %(statistics.mean(crossVal)))
 
 
-    clf = svm.SVC(kernel="linear", C = 1.0)
-    clf.fit(X[:-test_size],y[:-test_size])
-
-    correct_count = 0
-    predictions = clf.predict(X)
-    print(predictions)
-    for x in range(1, test_size+1):
-        if (predictions[x] == y[x]):
-            correct_count += 1
-
-
-    print("Accuracy:", (correct_count/test_size) * 100.00)
+    # test_size = int(floor(len(X)*0.2))
+    # print(len(X))
+    #
+    #
+    # clf = svm.SVC(kernel="linear", C = 1.0)
+    # clf.fit(X[:-test_size],y[:-test_size])
+    #
+    # correct_count = 0
+    # predictions = clf.predict(X)
+    # print(predictions)
+    # for x in range(0, test_size):
+    #     if (predictions[x+len(X)-test_size] == y[x+len(X)-test_size]):
+    #         correct_count += 1
+    #
+    #
+    # print("Accuracy:", (correct_count/test_size) * 100.00)
 
 
 Analysis()
